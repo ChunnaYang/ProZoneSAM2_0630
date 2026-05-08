1c21914110d928ee3368b5bc0b20129012d85a82
Chunna Yang <chunnay2000@gmail.com>
2026-05-08 17:50:17 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -247,7 +247,7 @@ export default function MedicalSAMDemo() {
         <img
           src="/深圳河套学院.png"
           alt="深圳河套学院"
-          className="absolute top-0 right-0 w-[672px] h-auto z-10 pointer-events-none"
+          className="absolute top-4 right-4 w-28 md:w-36 h-auto z-20 opacity-95"
         />
 
         {/* Header */}
